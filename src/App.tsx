b2474070934dabd58d1466a2051fa96140c6ecbf
gyeolhwi <gyeori0626@gmail.com>
2026-04-19 16:05:42 +0900

    REFACTOR(app): 미사용 darkTheme import 제거 및 포맷 정리 - 2026-04-19
    darkTheme 제거 후 남아있던 미사용 import 삭제
    ThemeProvider 삼항 연산자 공백 정리
    
    Author: gyeolhwi

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from "styled-components";
+import { MeshBackground } from "./components/common/MeshBackground";
 import AppRoutes from "./router";
 import { useThemeStore } from "./store/useThemeStore";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import { darkTheme, darkThemeForest, lightTheme } from "./styles/theme";
-import { MeshBackground } from "./components/common/MeshBackground";
+import { darkThemeForest, lightTheme } from "./styles/theme";
 
 function App() {
   const theme = useThemeStore((s) => s.mode);
 
   return (
-    <ThemeProvider theme={theme === "dark" ? darkThemeForest: lightTheme}>
+    <ThemeProvider theme={theme === "dark" ? darkThemeForest : lightTheme}>
       <GlobalStyle />
       <MeshBackground />
       <AppRoutes />
